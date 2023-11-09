4328e18e17e825670a75b02d4ef0e5845b30327f
yutaro119 <nakayama.yutarotu2@gmail.com>
2023-11-09 10:15:28 +0900

    update main.wasm

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Go Assemblt Tutorial.")
+	b, err := os.ReadFile("go.mod")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(b))
 }
